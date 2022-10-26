b082fa12849f498731fa71b0ff629c45f7c412d1
yanzayd <nelsonnyzbedel@gmail.com>
2022-10-26 18:00:16 +0200

    modifications for the calllenge

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,58 +1,58 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
-import Link from 'next/link'
-import React from 'react'
-import { Cursor, useTypewriter } from 'react-simple-typewriter'
-import { urlFor } from '../sanity'
-import { PageInfo } from '../typings'
-import BackgroundCircles from './BackgroundCircles'
+import Link from "next/link";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { urlFor } from "../sanity";
+import { PageInfo } from "../typings";
+import BackgroundCircles from "./BackgroundCircles";
 
 type Props = {
-  pageInfo: PageInfo
-}
+  pageInfo: PageInfo;
+};
 
 export default function Hero({ pageInfo }: Props) {
   const [text, content] = useTypewriter({
     words: [
       `Hi, I'm ${pageInfo?.name}`,
-      "Guy-who-loves-Coffee.tsx",
+      "Guy-from-DRC 🇨🇩 ",
       "<ButLovesToCodeMore />",
     ],
     loop: true,
     delaySpeed: 2000,
-  })
+  });
   return (
-    <div className='h-screen flex flex-col space-y-12 items-center justify-center text-center overflow-hidden '>
+    <div className="h-screen flex flex-col space-y-12 items-center justify-center text-center overflow-hidden ">
       <BackgroundCircles />
       <img
-        className='relative rounded-full h-32 w-32 mx-auto object-cover'
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src={urlFor(pageInfo?.heroImage).url()}
       />
-      <div className='z-20'>
+      <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
           {pageInfo?.role}
         </h2>
-        <h1 className='text-xl lg:text-6xl font-semibold px-10'>
-          <span className='mr-3'>
+        <h1 className="text-xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">
             {text}
             <Cursor cursorColor="#F7AB0A" />
           </span>
         </h1>
-        <div className='mt-20'>
+        <div className="mt-20">
           <Link href="#about">
-            <button className='heroButton'>About</button>
+            <button className="heroButton">About</button>
           </Link>
           <Link href="#experience">
-            <button className='heroButton'>Experience</button>
-          </Link>  
+            <button className="heroButton">Experience</button>
+          </Link>
           <Link href="#skills">
-            <button className='heroButton'>Skills</button>
-          </Link> 
-         <Link href="#projects">
-           <button className='heroButton'>Projects</button>
-         </Link>
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
