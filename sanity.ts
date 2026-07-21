4b0402b3093e7323fb4fe93a87f994356d72759b
bdl <bdl@MacBook-Pro-de-bdl-2.local>
2026-07-21 03:22:40 +0100

    fix: disable sanity cdn during build

--- sanity.ts
+++ sanity.ts
@@ -6,7 +6,7 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion: '2021-10-21',
-    useCdn: process.env.NODE_ENV === 'production',
+    useCdn: false,
 };
 
 //Set upt the client for fetching data in the getProps page functions
